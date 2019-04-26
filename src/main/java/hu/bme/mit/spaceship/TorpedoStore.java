693bca7c43c2fa7c138dad6d0492d06424810c49
Koppa Péter <koppa96@windowslive.com>
2019-04-26 09:32:41 +0200

    Adding more detailed exception message

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -32,7 +32,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("There are not enough torpedos to fire");
     }
 
     boolean success = false;
